d06a0b5686f28bce80c1ad088e412341f31b7a33
ANSH KUMAR <anshkumar1399@gmail.com>
2025-06-07 19:20:30 +0530

    week1

---
+++ week1js.js
@@ -0,0 +1,87 @@
+// Simple primitives 
+// normal syntax if-else loop
+
+// Q1 Write the program to greet a person given their rst and last name
+
+//let fname = `Ansh`;
+//let lname = `Kumar`;
+//console.log(`Hei I am ${fname} ${lname} call me ${fname[0]}${lname[0]}`)
+
+
+
+//Q2 Write a program that greets a person based on their gender. (If else)
+
+//let fname = `Ansh`;
+//let lname = `Kumar`;
+//let isMale = true;
+//if (isMale){
+//  console.log(`Hei boy ${fname}`)
+//}
+//else{
+//  console.log(`Hei girl ${fname}`)
+//}
+
+
+//Q3 Write a program that counts from 0 - 100 and prints (for loop)
+
+//let count = 0;
+//let sum = 0;
+//let mulFive = 0;
+//let allNum = '';
+//for (let i = 0; i<=100;i++){
+//  allNum += i + ', ';
+//  count++;
+//  sum+=i;
+//  if (i%5==0){
+//    mulFive++;
+//  }
+//}
+//console.log(allNum);
+//console.log(count);
+//console.log(sum);
+//console.log(mulFive);
+
+
+
+
+
+
+//Complex primitives
+//Array and Object
+
+//Q1 Write a program prints all the even numbers in an array
+
+//let numArr = [50, 20, 58, 85, 69, 96, 35, 25, 12, 7, 23, 30, 8, 1]
+//const arrLen = numArr.length;
+//let count = 0;
+//let evenArr = []
+//for (let i = 0; i<=arrLen; i++){
+//  if(numArr[i]%2==0){
+//    evenArr += numArr[i] + ' ';
+//    count++;
+//  }
+//}
+//console.log(evenArr);
+//console.log(count);
+
+
+//Q2 Write a program to print the biggest number in an arrya
+
+//let numArr = [50, 20, 58, 85, 69, 96, 35, 25, 12, 7, 23, 30, 8, 1]
+//const arrLen = numArr.length;
+//let bigNum = numArr[0]
+//for (let i = 0; i<=arrLen; i++){
+//  if(bigNum<numArr[i]){
+//    bigNum = numArr[i];
+//  }
+//}
+//console.log(bigNum);
+
+
+//Q3 Write a program that prints all the male people’s rst name given a complex object
+
+
+
+//Q4 Write a program that reverses all the elements of an array
+
+
